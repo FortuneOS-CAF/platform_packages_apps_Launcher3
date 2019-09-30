af002bd686cbd5e9266a6485fff80c375a638405
Chris Crump <chris@cypheros.co>
2019-09-29 21:17:44 -0400

    Launcher3: Create Fortune Launcher
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I33786e5706b5476e5a14c32e96229163f5ffd554

---
+++ quickstep/src/org/fortune/launcher/FortuneQuickstepLauncher.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020-2024 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.fortune.launcher
+
+import com.android.launcher3.uioverrides.QuickstepLauncher
+
+class FortuneQuickstepLauncher : QuickstepLauncher() {
+
+    companion object {
+        private const val TAG = "FortuneQuickstepLauncher"
+    }
+
+}
---
+++ src/org/fortune/launcher/FortuneLauncher.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020-2024 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.fortune.launcher
+
+import com.android.launcher3.Launcher
+
+class FortuneLauncher : Launcher() {
+
+    companion object {
+        private const val TAG = "FortuneLauncher"
+    }
+
+}
