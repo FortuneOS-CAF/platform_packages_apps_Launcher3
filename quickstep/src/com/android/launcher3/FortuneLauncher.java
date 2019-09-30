99665523c5ea010c6f52ff259fc39a754f960f24
Chris Crump <chris@cypheros.co>
2019-09-29 21:17:44 -0400

    Create Fortune Launcher
    Change-Id: I33786e5706b5476e5a14c32e96229163f5ffd554

---
+++ quickstep/src/com/android/launcher3/FortuneLauncher.java
@@ -0,0 +1,12 @@
+/*
+ *  Copyright (C) 2024 FortuneOS
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.launcher3;
+
+import com.android.launcher3.uioverrides.QuickstepLauncher;
+
+public class FortuneLauncher extends QuickstepLauncher {
+
+}
